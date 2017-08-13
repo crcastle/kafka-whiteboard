5576e092195ca2e2cecff547042e14390ec5e4d6
Chris Castle <castle@heroku.com>
2017-08-13 14:30:07 -0700

    Bug fixes for touch support

--- src/browser/app.js
+++ src/browser/app.js
@@ -30,24 +30,32 @@ var upEvent = isTouchSupported
     : isMSPointerSupported ? "MSPointerUp" : "mouseup";
 
 painting.addEventListener(downEvent, e => {
-  Object.assign(brush, { down: true, x: e.clientX, y: e.clientY });
+  e.preventDefault();
+  var x = isTouchSupported ? e.targetTouches[0].clientX : e.clientX;
+  var y = isTouchSupported ? e.targetTouches[0].clientY : e.clientY;
+  Object.assign(brush, { down: true, x: x, y: y });
 });
 painting.addEventListener(upEvent, e => {
+  e.preventDefault();
   Object.assign(brush, { down: false });
 });
 painting.addEventListener("mouseleave", e => {
   Object.assign(brush, { down: false });
 });
 painting.addEventListener(moveEvent, e => {
+  e.preventDefault();
   if (!brush.down) return;
+
+  var x = isTouchSupported ? e.targetTouches[0].clientX : e.clientX;
+  var y = isTouchSupported ? e.targetTouches[0].clientY : e.clientY;
   strokes.push({
     color: brush.color,
     x0: brush.x,
     y0: brush.y,
-    x1: e.clientX,
-    y1: e.clientY
+    x1: x,
+    y1: y
   });
-  Object.assign(brush, { x: e.clientX, y: e.clientY });
+  Object.assign(brush, { x: x, y: y });
   paint(strokes);
 });
 socket.addEventListener("paint", paint);
@@ -71,6 +79,5 @@ function paint(segments) {
 
 function transmit() {
   if (!strokes.length) return;
-  console.log(strokes.slice(0));
   socket.emit("strokes", strokes.splice(0));
 }
